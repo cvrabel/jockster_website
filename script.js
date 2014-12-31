0b3feab989a82498b6a900ccccb5f23e9435e11d
chris <cjv38@cornell.edu>
2014-12-31 16:22:23 -0500

    url retrieve commit

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-function addTeam() {
-    var myurl = "https://erikberg.com/nba/teams.json";
-    $.ajax(({
-        url: myurl,
-        dataType: 'jsonp',
-        success: 
-        function (data) {
-            console.log(data);
-            var teamlist = [];
-            for (var elem in data){
-                console.log(elem.full_name);
-                teamlist.push(elem.full_name);
-            }
-        }
-    }));
+
+function addTeam(){
+    alert('working');
+$.ajax({
+  url      : 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent('http://nba.com/rss/nba_rss.xml'),
+  dataType : 'jsonp',
+  success  : function (data) {
+   
+      $.each(data.responseData.feed.entries, function (i, e) {
+        console.log("------------------------");
+        console.log("title      : " + e.title);
+        console.log("link     : " + e.link);
+        console.log("description: " + e.description);
+      });
+    
+  }
+});
 }
+
